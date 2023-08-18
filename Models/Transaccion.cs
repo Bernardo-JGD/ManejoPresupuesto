9664e120d941ed3c28f5cb0e9311a22bbaed9736
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2023-08-17 19:03:16 -0600

    Primer commit, subiendo todo el avance del proyecto

---
+++ Models/Transaccion.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Transaccion
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        [Display(Name = "Fecha Transacción")]
+        [DataType(DataType.DateTime)]
+        public DateTime FechaTransaccion { get; set; } = DateTime.Today;
+        //DateTime.Parse(DateTime.Now.ToString("g"))
+        //yyyy-MM-dd hh:MM tt es lo mismo que esto "g"
+        //DateTime.Now -- Agrega la fecha de hoy por defecto
+        //Today la fecha exacta
+        //Now, la fecha con hora exacta
+        public decimal Monto { get; set; }
+
+        [Range(1, maximum: int.MaxValue, ErrorMessage = "Debe seleccionar una categoria")]
+        [Display(Name = "Cateogría")]
+        public int CategoriaId { get; set; }
+        
+        [StringLength(maximumLength: 1000, ErrorMessage = "La nota no puede pasar de {1} caracteres")]
+        public string Nota { get; set; }
+
+        [Range(1, maximum: int.MaxValue, ErrorMessage = "Debe seleccionar una cuenta")]
+        [Display(Name = "Cuenta")]
+        public int CuentaId { get; set; }
+
+        [Display(Name = "Tipo Operación")]
+        public TipoOperacion TipoOperacionId { get; set; } = TipoOperacion.Ingreso;
+
+        public string? Cuenta { get; set; }
+        public string? Categoria { get; set; }
+
+    }
+}
---
+++ wwwroot/js/FormularioTransacciones.js
@@ -0,0 +1,20 @@
+﻿function inicializarFormularioTransacciones(urlObtenerCategorias) {
+    $("#TipoOperacionId").change(async function () {
+        const valorSeleccionado = $(this).val();
+
+        const respuesta = await fetch(urlObtenerCategorias, {
+            method: 'POST',
+            body: valorSeleccionado,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const json = await respuesta.json();
+        console.log(json);
+        //Estas opciones rellenan el Select de categorias
+        //.map itera el json´y acomoda cada objeto en una etiqueta option
+        const opciones = json.map(categoria => `<option value=${categoria.value} >${categoria.text}</option>`);
+        $("#CategoriaId").html(opciones);
+    })
+}
